2caa6029278181646a73f5967e5a86a7b12a3651
Álvaro Enuel Serein <40794855+aeserein@users.noreply.github.com>
2019-08-21 11:28:21 -0300

    Add files via upload

---
+++ Clase_02.Entidades/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase_02.Entidades {
+
+    public class Sello {
+
+        public static string mensaje;
+        public static ConsoleColor colorFondo, colorLetra;
+
+        public static string Imprimir() {
+            return Sello.mensaje;
+        }
+
+        public static void ImprimirEnColor() {
+            Console.BackgroundColor = colorFondo;
+            Console.ForegroundColor = colorLetra;
+            Console.WriteLine(Sello.mensaje);
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+
+        public static void Borrar() {
+            Sello.mensaje = "";
+        }
+
+    }
+}
