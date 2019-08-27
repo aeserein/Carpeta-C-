490c555a414d3fac9434546e0ac42b38e867a9ef
aeserein <ae.serein@gmail.com>
2019-08-27 02:47:02 -0300

    Ejercicios_hasta_14

---
+++ Serein.Álvaro/Ejercicio_13_lib/Class1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Ejercicio_13_lib {
+
+    public class Conversor {
+
+        public static string DecimalABinario(double numero) {
+
+            // no trabaja con parte decimal
+            // y ni siquiera existe binario con coma
+            // acepta tipo doble por consigna pero no funciona con numeros con coma
+
+            string binario = "";
+            int parteEntera;
+            int len = 0;
+
+            while (numero>=1) {
+                binario = (int)numero % 2 + binario;
+                parteEntera = (int)(numero /= 2);
+            }
+
+            len = binario.Length;
+
+            while (len%8 > 0) {
+                binario = '0' + binario;
+                len = binario.Length;
+            }
+
+            return binario;
+
+        }
+
+        public static double BinarioADecimal(string binario) {
+
+            double numero=0;
+
+            int f, exponente;
+            exponente = binario.Length;
+
+            for (f=0 ; f<binario.Length; f++) {
+
+                exponente--;
+                if (binario[f] == '1')
+                    numero += Math.Pow(2, exponente);
+            }
+
+            return numero;  //devuelvo un long que se convierte a doble
+
+        }
+
+    }
+
+}
